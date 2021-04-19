db347b557a50055fe466055b95a68985847e43da
ragnar17 <edwardamit17@gmail.com>
2021-04-19 19:14:34 +0530

    fixing

--- src/components/Pa1.js
+++ src/components/Pa1.js
@@ -103,7 +103,6 @@ class Pa1 extends React.Component {
         })
     };
     fetch("https://ragnar177.pythonanywhere.com/des-avalanche",requestOptions).then(response=>response.json()).then(data =>{
-      // console.log(data)
       this.setState({
         graphdata : data.graphdata
       })
